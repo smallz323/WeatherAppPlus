bef6badfe42a4c35eadd098face30c3a1ac72a6b
smallz323 <74576252+smallz323@users.noreply.github.com>
2021-01-05 20:56:26 -0800

    silly changes

--- script.js
+++ script.js
@@ -13,18 +13,22 @@ if (minutes < 10) {minutes = `0${minutes}`
 clock.innerHTML = `${day} ${hour}:${minutes}`;
 
 
+
 function showTemperature(response) {
-  let temperatureElement =
-    document.querySelector("#header-temperature");
-      temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#header-temperature");
   document.querySelector("#header-city").innerHTML = response.data.name;
   document.querySelector("#humid").innerHTML = `Humidity: ` + response.data.main.humidity + `%`;
-  document.querySelector("#feels-like").innerHTML = `Feels like: ` + Math.round(response.data.main.feels_like) + `⁰F`
+  document.querySelector("#feels-like").innerHTML = `Feels like: ` + Math.round(response.data.main.feels_like) + `⁰F`;
   document.querySelector("#weather-icon-text").innerHTML = response.data.weather[0].description;
-  let weatherIcon = response.data.weather[0].icon;
-  document.querySelector("#wind").innerHTML = `Wind speed: `+ Math.round (response.data.wind.speed) + `m/h`
-  document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`)
+  document.querySelector("#wind").innerHTML = `Wind speed: ` + Math.round(response.data.wind.speed) + `m/h`;
+  document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`).innerHTML =response.data.weather[0].icon;
+  
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  
 }
+
+  
   function showSearch(event) {
     event.preventDefault();
     let city = document.querySelector("#city-input-search").value;
@@ -37,14 +41,14 @@ function showTemperature(response) {
 
 function celsiusTemperature(event) {
   event.preventDefault();
-  let celsisusDisplayTemperature = Math.round((70 - 32) * 5/9) ;
+  let celsisusDisplayTemperature = Math.round((62 - 32) * 5/9) ;
   let temperatureElement = document.querySelector("#header-temperature");
   temperatureElement.innerHTML = celsisusDisplayTemperature;
 }
+
+
   let weatherButton = document.querySelector("#search-city");
 weatherButton.addEventListener("submit", showSearch);
   
 let celsiusConvert = document.querySelector("#celsius-convert");
-celsiusConvert.addEventListener("click", celsiusTemperature)
-
- 
+celsiusConvert.addEventListener("click", celsiusTemperature);
